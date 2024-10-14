019ceab27f162414a4117586bbfeb7383188447a
Oscar Reyes <ooreyes.ro@gmail.com>
2024-10-14 20:24:14 -0300

    add method to do a post, add test to verify that

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 import express from 'express';
 import fs, { readFileSync } from "fs";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
 const readData =() => {
     try{
@@ -12,7 +14,7 @@ const readData =() => {
     }
 }
 
-const writeData = () => {
+const writeData = (data) => {
     try{
         fs.writeFileSync("./db.json", JSON.stringify(data))
     } catch (error){
@@ -30,6 +32,18 @@ app.get("/books/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const book = data.books.find((book) => book.id === id);
     res.json(book);
+});
+
+app.post("/books", (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const newBook = {
+        id: data.books.length + 1,
+        ...body,
+    };
+    data.books.push(newBook);
+    writeData(data);
+    res.json(newBook);
 });
 
 app.get("/", (req, res) =>{
